60f44fb1c4d2602fa5bf8ef852b74946c8290577
Matthew Brett <matthew.brett@gmail.com>
2022-09-08 18:36:44 +0100

    MRG: project instructions, outlier code
    See the file `on_the_project.md` for more instructions on how to use
    this PR.

--- findoutlie/outfind.py
+++ findoutlie/outfind.py
@@ -3,9 +3,33 @@
 
 from pathlib import Path
 
+import numpy as np
+
+import nibabel as nib
+
+from .metrics import dvars
+from .detectors import iqr_detector
+
 
 def detect_outliers(fname):
-    return [42]
+    """ Detect outliers given image file path `filename`
+
+    Parameters
+    ----------
+    fname : str or Path
+        Filename of 4D image, as string or Path object
+
+    Returns
+    -------
+    outliers : array
+        Indices of outlier volumes.
+    """
+    # This is a very simple function, using dvars and iqroutliers
+    img = nib.load(fname)
+    dvs = dvars(img)
+    is_outlier = iqr_detector(dvs, iqr_proportion=2)
+    # Return indices of True values from Boolean array.
+    return np.nonzero(is_outlier)
 
 
 def find_outliers(data_directory):
